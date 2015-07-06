49ec5dfb8312dbe25a98fcc08be94e001feae9e4
Graham Wakefield <grrrwaaa@gmail.com>
2015-07-06 15:05:22 +0900

    site update

--- alife/al.js
+++ alife/al.js
@@ -45,11 +45,13 @@ var max = Math.max;
 
 var piover2 = Math.PI/2.;
 
+/*
 function mouse(event, button, x, y) {}
 function key(event, k) {}
 function update() {}
 function draw(ctx) {}
 function reset() {}
+*/
 
 ////////////////////////////////////////////////////////////////////////////////
 
