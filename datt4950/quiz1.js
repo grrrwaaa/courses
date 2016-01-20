f36fc0a592af7047e3f55662a7ff8a3229770bc1
Graham Wakefield <grrrwaaa@gmail.com>
2016-01-20 01:01:15 -0500

    site update

---
+++ datt4950/quiz1.js
@@ -0,0 +1,48 @@
+var quiztext = '<p>What class of CA did Stephen Wolfram identify as leading to complex behaviour?</p> <ul><li><input type="radio" name="entry.1825937145" value="Class 1" id="group_1825937145_1" role="radio"  aria-label="Class 1">Class 1</li> <li><input type="radio" name="entry.1825937145" value="Class 2" id="group_1825937145_2" role="radio"  aria-label="Class 2">Class 2</li> <li><input type="radio" name="entry.1825937145" value="Class 3" id="group_1825937145_3" role="radio"  aria-label="Class 3">Class 3</li> <li><input type="radio" name="entry.1825937145" value="Class 4" id="group_1825937145_4" role="radio"  aria-label="Class 4">Class 4</li></ul><p>In the Game of Life transition rule, how many input cells influence the output cell state?</p> <ul><li><input type="radio" name="entry.92380568" value="1" id="group_92380568_1" role="radio"  aria-label="1">1</li> <li><input type="radio" name="entry.92380568" value="4" id="group_92380568_2"role="radio"  aria-label="4">4</li> <li><input type="radio" name="entry.92380568" value="8" id="group_92380568_3" role="radio"  aria-label="8">8</li> <li><input type="radio" name="entry.92380568" value="9" id="group_92380568_4" role="radio"  aria-label="9">9</li> <li><input type="radio" name="entry.92380568" value="All cells in the space" id="group_92380568_5" role="radio"  aria-label="All cells in the space">All cells in the space</li></ul><p>Which of these is an example of stochastic/probabilistic cellular automata?</p> <ul><li><input type="radio" name="entry.816744167" value="Game of Life" id="group_816744167_1" role="radio"  aria-label="Game of Life">Game of Life</li> <li><input type="radio" name="entry.816744167" value="Langton&#39;s Ant" id="group_816744167_2" role="radio"  aria-label="Langton&#39;s Ant">Langton&#39;s Ant</li> <li><input type="radio" name="entry.816744167" value="Brian&#39;s Brain" id="group_816744167_3" role="radio"  aria-label="Brian&#39;s Brain">Brian&#39;s Brain</li> <li><input type="radio" name="entry.816744167" value="Forest Fire" id="group_816744167_4" role="radio"  aria-label="Forest Fire">Forest Fire</li> <li><input type="radio" name="entry.816744167" value="Critters (a block-rule CA)" id="group_816744167_5" role="radio"  aria-label="Critters (a block-rule CA)">Critters (a block-rule CA)</li></ul><p>Which of these is NOT an essential component of a cellular automata?</p> <ul><li><input type="radio" name="entry.1552256468" value="Transition rule" id="group_1552256468_1" role="radio"  aria-label="Transition rule">Transition rule</li> <li><input type="radio" name="entry.1552256468" value="Cellular space" id="group_1552256468_2" role="radio"  aria-label="Cellular space">Cellular space</li> <li><input type="radio" name="entry.1552256468" value="Initial conditions" id="group_1552256468_3" role="radio"  aria-label="Initial conditions">Initial conditions</li> <li><input type="radio" name="entry.1552256468" value="Colour space" id="group_1552256468_4" role="radio"  aria-label="Colour space">Colour space</li> <li><input type="radio" name="entry.1552256468" value="Time axis" id="group_1552256468_5" role="radio"  aria-label="Time axis">Time axis</li> <li><input type="radio" name="entry.1552256468" value="Neighbourhood" id="group_1552256468_6" role="radio"  aria-label="Neighbourhood">Neighbourhood</li> <li><input type="radio" name="entry.1552256468" value="Boundary conditions" id="group_1552256468_7" role="radio"  aria-label="Boundary conditions">Boundary conditions</li> <li><input type="radio" name="entry.1552256468" value="Cell states" id="group_1552256468_8" role="radio"  aria-label="Cell states">Cell states</li></ul><p>The Grey-Scott Model is an example of:</p> <ul><li><input type="radio" name="entry.1125157886" value="A mobile CA" id="group_1125157886_1" role="radio"  aria-label="A mobile CA">A mobile CA</li> <li><input type="radio" name="entry.1125157886" value="A particle CA" id="group_1125157886_2" role="radio"  aria-label="A particle CA">A particle CA</li> <li><input type="radio" name="entry.1125157886" value="A reaction-diffusion system" id="group_1125157886_3" role="radio"  aria-label="A reaction-diffusion system">A reaction-diffusion system</li> <li><input type="radio" name="entry.1125157886" value="An asynchronous CA" id="group_1125157886_4" role="radio"  aria-label="An asynchronous CA">An asynchronous CA</li></ul><p>Which of these CA components CANNOT be made approximately continuous?</p> <ul><li><input type="radio" name="entry.302949864" value="Cell states" id="group_302949864_1" role="radio"  aria-label="Cell states">Cell states</li> <li><input type="radio" name="entry.302949864" value="Transition rules" id="group_302949864_2" role="radio"  aria-label="Transition rules">Transition rules</li> <li><input type="radio" name="entry.302949864" value="Neighborhood" id="group_302949864_3" role="radio"  aria-label="Neighborhood">Neighborhood</li> <li><input type="radio" name="entry.302949864" value="Time axis" id="group_302949864_4" role="radio"  aria-label="Time axis">Time axis</li> <li><input type="radio" name="entry.302949864" value="None of the above (they can all be made&nbsp;approximately&nbsp;continuous)" id="group_302949864_5" role="radio"  aria-label="None of the above (they can all be made&nbsp;approximately&nbsp;continuous)">None of the above (they can all be made approximately continuous)</li></ul><p>Why did von Neumann use Cellular Automata (in the late 1940&#39;s)?</p> <ul><li><input type="radio" name="entry.782083901" value="To emulate the patterns on animals and shells" id="group_782083901_1" role="radio"  aria-label="To emulate the patterns on animals and shells">To emulate the patterns on animals and shells</li> <li><input type="radio" name="entry.782083901" value="For the design of computer hardware" id="group_782083901_2" role="radio"  aria-label="For the design of computer hardware">For the design of computer hardware</li> <li><input type="radio" name="entry.782083901" value="To demonstrate a machine that can reproduce itself" id="group_782083901_3" role="radio"  aria-label="To demonstrate a machine that can reproduce itself">To demonstrate a machine that can reproduce itself</li> <li><input type="radio" name="entry.782083901" value="To model elementary particle physics" id="group_782083901_4" role="radio"  aria-label="To model elementary particle physics">To model elementary particle physics</li> <li><input type="radio" name="entry.782083901" value="To make things in Minecraft" id="group_782083901_5" role="radio"  aria-label="To make things in Minecraft">To make things in Minecraft</li></ul>';
+
+$(document).ready(function(){
+
+	$("#quizbody").html(quiztext);
+
+	function reset_form() { document.getElementById("ss-form").reset(); }
+	$(window).blur(function(){ reset_form(); });	
+	// full-screen available?
+	if (document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled
+	) {
+		// let start button open fullscreen:
+		var quiz = document.getElementById("quiz");
+		$("#start").click(function() {	
+			// go full-screen			
+			if (quiz.requestFullscreen) {
+				quiz.requestFullscreen();
+			} else if (quiz.webkitRequestFullscreen) {
+				quiz.webkitRequestFullscreen();
+			} else if (quiz.mozRequestFullScreen) {
+				quiz.mozRequestFullScreen();
+			} else if (quiz.msRequestFullscreen) {
+				quiz.msRequestFullscreen();
+			}
+		});		
+		// when fullscreen happens:
+		function FShandler(b) { 
+			if (document.fullscreen
+			 || document.mozFullScreen
+			 || document.webkitIsFullScreen
+			 || document.msFullscreenElement) {
+				//console.log("fullscreen on");
+				reset_form();
+				$("#ss-form").show();
+			} else {
+				//console.log("fullscreen off");
+				reset_form();
+				$("#ss-form").hide();
+			}
+		}	
+		document.addEventListener("fullscreenchange", FShandler);
+		document.addEventListener("webkitfullscreenchange", FShandler);
+		document.addEventListener("mozfullscreenchange", FShandler);
+		document.addEventListener("MSFullscreenChange", FShandler);
+	} else {
+		document.write("<p>This page requires a browser with full screen support.</p>");
+	}
+});
